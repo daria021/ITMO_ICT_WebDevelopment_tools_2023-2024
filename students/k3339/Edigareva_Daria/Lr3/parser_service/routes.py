1cc1f3225d452fb235bfb4f1314a8a60a18ae3d3
daria021 <dariaedigareva@gmail.com>
2025-06-02 13:35:13 +0300

    fix

--- students/k3339/Edigareva_Daria/Lr3/parser_service/routes.py
+++ students/k3339/Edigareva_Daria/Lr3/parser_service/routes.py
@@ -11,11 +11,11 @@ from responses import FetchResponse
 router = APIRouter()
 
 
-@router.get("/fetch", response_model=FetchResponse, summary="Скачать страницу и вернуть HTML")
-async def fetch(query: Annotated[FetchQuery, Query(...)]):
+@router.get("/fetch", summary="Скачать страницу и вернуть HTML")
+async def fetch(query: Annotated[FetchQuery, Query(...)]) -> FetchResponse:
     try:
         title, html = await fetch_html_and_title(str(query.url))
-    except Exception as exc:  # noqa: BLE001
+    except Exception as exc:
         raise HTTPException(status_code=502, detail=str(exc)) from exc
 
     return FetchResponse(url=query.url, title=title, html=html)
